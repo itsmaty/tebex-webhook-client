f59211792b6c2e773ade2689cf62bd0364247753
Matthias <matthiasnordgauer@gmail.com>
2024-08-14 00:44:26 +0200

    chore(types): create type for all webhook types

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,3 +7,18 @@ export interface ITebexWebhookClientOptions{
   endpoint?: string,
   ips?: string[];
 }
+
+export type TebexWebhookEvent = 
+'payment.completed' | 
+'payment.declined' | 
+'payment.refunded' | 
+'payment.dispute.opened' | 
+'payment.dispute.won' | 
+'payment.dispute.lost' | 
+'payment.dispute.closed' | 
+'recurring-payment.started' | 
+'recurring-payment.renewed' | 
+'recurring-payment.ended' | 
+'recurring-payment.cancellation.requested' | 
+'recurring-payment.cancellation.aborted' | 
+'validation.webhook';
