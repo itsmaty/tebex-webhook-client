f0d6ffdffd4bfdcc8e941da0a5db5650cc8375b3
Matthias <matthiasnordgauer@gmail.com>
2024-08-14 14:39:41 +0200

    feat(core): base class with all necessary fields

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { Application, Router } from "express";
+import { ITebexWebhookClientOptions } from "./types";
+
+export default class TebexWebhookClient {
+
+  /* the tebex webhook secret for validating signatures */
+  private readonly secret: string;
+
+  /* the express app or router to use */
+  private readonly express: Application | Router | undefined;
+
+  /* the port for the internal express server to listen on, if used */
+  private readonly port: number = 80;
+
+  /* the endpoint/route for incoming webhooks */
+  private readonly endpoint: string = '/webhook';
+
+  /* valid ip adresses for the webhooks */
+  private readonly ips: string[] = ['129.213.15.18', '192.168.1.1'];
+
+  /* if logging/console prints should be enabled */
+  private logging: boolean = false;
+
+  /* the callback functions/subscribers for the events */
+  private EventSubscribers: {[key: string]: Function[]} = {};
+
+  constructor(options: ITebexWebhookClientOptions) {
+    
+  }
+}
