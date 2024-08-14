bc7b5b09d641880147481766f322c67a22718dc9
Matthias <matthiasnordgauer@gmail.com>
2024-08-14 14:01:21 +0200

    chore(utils): add return type Application to CreateExpressServer utility

--- src/utils/CreateExpressServer.ts
+++ src/utils/CreateExpressServer.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { Application } from 'express';
 
-export default function CreateExpressServer(port: number) {
+export default function CreateExpressServer(port: number): Application {
   const ExpressServer: Application = express();
 
   ExpressServer.listen(port)
